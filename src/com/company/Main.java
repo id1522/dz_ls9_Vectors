f0cf9ce3bd08c7bd873f918005624cf345e88337
Nikita Smorschok <nsmorschok@gmail.ru>
2022-03-29 19:06:11 +0300

    Vectors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,40 @@
 package com.company;
+
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Vector;
+
 public class Main {
 
     public static void main(String[] args) {
-        Vector a = new Vector(13, 25, 4);
-        Vector b = new Vector(43, -7, 11);
+        Vector2d v1 = new Vector2d(5.0, 6.0);
+        Vector2d v2 = new Vector2d(2.0, 3.0);
+        Vector3d v3 = new Vector3d(5.0, 6.0, 3.0);
+        Vector3d v4 = new Vector3d(2.0, 3.0, 4.0);
+        System.out.println("Vector2d 1 " + v1);
+        System.out.println("Vector2d 2 " + v2);
+        System.out.println("Vector2d 1 Length " + v1.calculateLength());
+        System.out.println("Vector2d 2 Length " + v2.calculateLength());
+        System.out.println("Vector2d  Scalar " + v1.calculateDotProduct(v2));
+        System.out.println("Vector2d  Sum " + v1.calculateSum(v2));
+        System.out.println("Vector2d  Diff " + v1.calculateDifference(v2));
+        System.out.println("Vector2d compareTo v1 and v2 " + v1.compareTo(v2));
+        System.out.println("Created Vectors " + Arrays.toString(Vector2d.createVectors(4)));
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("Vector3d 3 " + v3);
+        System.out.println("Vector3d 4 " + v4);
+        System.out.println("Vector3d 3 Length " + v3.calculateLength());
+        System.out.println("Vector3d 4 Length " + v4.calculateLength());
+        System.out.println("Vector3d  Scalar " + v3.calculateDotProduct(v4));
+        System.out.println("Vector3d  Sum " + v3.calculateSum(v4));
+        System.out.println("Vector3d  Diff " + v3.calculateDifference(v4));
+        System.out.println("Vector3d compareTo v3 and v4 " + v3.compareTo(v4));
+        System.out.println("Created Vectors3d " + Arrays.toString(Vector3d.createVectors(4)));
 
-        ViewResult.showScalarResult(a.scalarProduct(b));
-        ViewResult.showVectorResult(a.vectorProduct(b));
+
     }
 }
 
-class ViewResult{
-    public static void showVectorResult(double[] result) {
-        System.out.print("Векторное произведение векторов:[a,b]=<");
-        for (int i = 0; i < result.length; i++) {
-            System.out.print(result[i] + (i != 2 ? ";" : ">\n"));
-        }
-    }
-
-    public static void showScalarResult(double d) {
-        System.out.println("Скалярное произведение векторов: " + d);
-    }
-}
 
 
 
--- src/com/company/Vector3d.java
+++ src/com/company/Vector3d.java
@@ -1,2 +1,80 @@
-package com.company;public class Vector3d {
+package com.company;
+
+import java.util.Random;
+
+public class Vector3d {
+    private double x;
+    private double y;
+    private double z;
+    public static final String DESCRIPTION1 = "\nЭто вектор для 3d системы координат";
+
+    public Vector3d(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Vector3d() {
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+    }
+
+    public double calculateLength() {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+
+    public double calculateDotProduct(Vector3d vector3d) {
+        return x * vector3d.getX() + y * vector3d.getY() + z * vector3d.getZ();
+    }
+
+    public Vector3d calculateSum(Vector3d vector3d) {
+        return new Vector3d(x + vector3d.getX(), y + vector3d.getY(), z + vector3d.getZ());
+    }
+
+    public Vector3d calculateDifference(Vector3d vector3d) {
+        return new Vector3d(x - vector3d.getX(), y - vector3d.getY(), z - vector3d.getZ());
+    }
+
+    public int compareTo(Vector3d vector3d) {
+        return (int) (this.calculateLength() - vector3d.calculateLength());
+    }
+
+    public java.lang.String toString() {
+        return DESCRIPTION1 + ": \nVector3d {" + " x=" + x + ", y=" + y + ", z=" + z + "}";
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public static Vector3d[] createVectors(int n) {
+        Random random = new Random();
+        Vector3d[] vector3ds = new Vector3d[n];
+        for (int i = 0; i < n; i++) {
+            vector3ds[i] = new Vector3d(random.nextDouble(),
+                    random.nextDouble(), random.nextDouble());
+        }
+        return vector3ds;
+    }
 }
